cd084109f9056e23a7095c9836650c7209c810c3
Yazalde Filimone <yazaldefilimon@gmail.com>
2025-09-03 11:04:30 +0200

    *

---
+++ src/types/maybe.ts
@@ -0,0 +1,9 @@
+export type Maybe<T> = T | null;
+
+export function isSome<T>(m: Maybe<T>): m is T {
+  return m !== null;
+}
+
+export function isNone<T>(m: Maybe<T>): m is null {
+  return m === null;
+}
